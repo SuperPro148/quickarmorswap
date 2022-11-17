b0f303876528d3f4c4b146dc56d055e8513d86ca
unknown <kymohendriks@gmail.com>
2022-11-17 23:23:53 +0100

    removed unused imports

--- src/main/java/io/github/superpro148/quickarmorswap/config/ModMenuIntegration.java
+++ src/main/java/io/github/superpro148/quickarmorswap/config/ModMenuIntegration.java
@@ -2,7 +2,6 @@ package io.github.superpro148.quickarmorswap.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import io.github.superpro148.quickarmorswap.config.QuickArmorSwapConfig;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
